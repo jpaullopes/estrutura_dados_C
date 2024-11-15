4c4e2bf1823cdbc84c377426111b767b29ea0b1a
jão <159722770+jpaullopes@users.noreply.github.com>
2024-11-15 10:56:10 -0300

    Update dinamicaEncadeada.c

--- listaDinamicaEncadeada/dinamicaEncadeada.c
+++ listaDinamicaEncadeada/dinamicaEncadeada.c
@@ -4,10 +4,6 @@
 //implementação das funções
 //cria uma lista vazia
 
-struct elemento{
-    Aluno dados;
-    struct elemento *proximo;
-};
 //cria uma lista vazia
 Lista* criarLista(void){
     Lista *li = (Lista*) malloc(sizeof(Lista));
@@ -18,16 +14,15 @@ Lista* criarLista(void){
 }
 //Liberar Lista
 void liberarLista(Lista* li){
-    if(li != NULL){
-        Lista *no;
+    if(li != NULL){//verifica se a lista é valida para ser liberada e se não está vazia
+        struct elemento *no; //cria um ponteiro auxiliar para percorrer a lista
         while ((*li) != NULL)
         {
             no = *li;
             *li = (*li)->proximo;
             free(no);
         }
-        free(li);
-        
+        free(li);     
     }
 }
 //calcular tamanho
@@ -38,10 +33,14 @@ int tamanhoLista(Lista *li){
     while (*li != NULL)
     {
         count++;
-        *no = (*no)->proximo;
+        no = (*no)->proximo;
     }
-    return count;
-    
+    return count;  
+}
+//Retornar o tamanho da lista(Usando recursividade)
+int tamanhoListaRecursivo(Lista li){
+    if(li == NULL) return 0;//Se lista estiver vazia ou se chegar no final da lista(NULL)
+    return 1 + tamanhoListaRecursivo(li->proximo);//chama a função recursivamente até que a lista seja NULL
 }
 //Saber se lista está vazia
 int listaVazia(Lista *li){
@@ -82,4 +81,127 @@ int inserirFinalLista(Lista *li, Aluno al){
     }
     return 1;
 }
+//inserção ordenada
+int inserirListaOrdenada(Lista *li, Aluno al){
+    if(li == NULL) return 0;
+    struct elemento *no = (struct elemento*)malloc(sizeof(struct elemento));
+    if(no == NULL) return 0;
+    no->dados = al;
+    //verificar se lista é vazia
+    if(listaVazia(li)){
+        no->proximo = (*li);
+        li = no;
+        return 1;
+    }
+    else{
+        struct elemento *atual, *anterior = *li;
+        while (atual != NULL && atual->dados.matricula < al.matricula)
+        {
+            anterior = atual;
+            atual = atual->proximo;
+        }
+        if(atual == *li){
+            no->proximo = (*li);
+            *li = no;
+        }
+        else{
+            no->proximo = anterior->proximo;
+            anterior->proximo = no;
+        }
+    }
 
+}
+//remoção no inicio da lista
+int removerComecoLista(Lista *li){
+    if(li == NULL) return 0;
+    if((*li) == NULL) return 0;
+    struct elemento *no = *li;
+    *li = no->proximo;
+    free(no);
+    return 1;
+}
+//remoção no final da lista
+int remocaoFinalLista(Lista *li){
+    if(listaVazia(li))return 0;
+    struct elemento *anterior, *no = *li;
+    while(no->proximo != NULL){ //até que proximo de no seja NULL
+        anterior = no;
+        no = no->proximo;
+    }
+    if(no == (*li)){ //caso seja o primeiro elemento que vá ser removido
+        *li = no->proximo;
+    }
+    else{
+        anterior->proximo = no->proximo; //anterior passa a apontar para NULL
+    }
+    free(no);
+    return 1;
+}
+//remoção lista ordenadamente com base na matricula
+int remocaoListaOrdenada(Lista *li, int mat){
+    if(li == NULL) return 0;
+    struct elemento *anterior, *no = *li;
+    while (no->proximo != NULL && no->dados.matricula != mat)
+    {
+        anterior = no;
+        no = no->proximo;
+    }
+    if(no == NULL) return 0; //caso a matricula não exista ou a lista esteja vazia
+    if(no == *li){ //caso seja o primeiro elemento
+        *li = no->proximo;
+    }
+    else{ //remove do meio ou do final
+        anterior->proximo = no->proximo; //anterior passa a apontar para o proximo de no
+    }
+    free(no);
+    return 1;
+}
+//consulta dados de um aluno com base na matricula
+int consultaListaMatricula(Lista *li, int mat, Aluno *al){
+    if(li == NULL) return 0;//verifica se a lista é valida
+    struct elemento *no = *li;
+    while (no != NULL && no->dados.matricula != mat)
+    {
+        no = no->proximo;
+    }
+    if(no == NULL) return 0; //a matricula não existe ou a lista está vazia
+    *al = no->dados;
+    return 1;
+}
+//consulta dados de um aluno com base na posição
+int consultaListaPosicao(Lista *li,int pos, Aluno *al){ //considerando posição começando de 01
+    if(li == NULL || pos <= 0) return 0;
+    struct elemento *no = *li;
+    int contador = 1;
+    while(no != NULL && contador < pos){
+        no = no->proximo;
+        contador++;
+    }
+    if(no == NULL) return 0; //posição não existe ou a lista está vazia
+    *al = no->dados;
+    return 1;
+}
+//exibe a lista
+void exibirLista(Lista li){
+    if(li == NULL) printf("Lista Vazia\n");
+    while (li->proximo != NULL)
+    {
+        printf("Matricula: %d\n", li->dados.matricula);
+        printf("Nome: %s\n", li->dados.nome);
+        li = li->proximo;
+    }
+}
+//Anexação de listas encadeadas
+int anexarListas(Lista *lista1, Lista *lista2){
+    if(lista1 == NULL || lista2 == NULL) return 0;
+    //verificar se as listas estão vazias
+    if(listaVazia(lista1)) return 0;
+    if(listaVazia(lista2)) return 0;
+    struct elemento *no = *lista1;
+    while (no->proximo != NULL)
+    {
+        no = no->proximo;
+    }
+    no->proximo = *lista2; //o proximo do ultimo elemento da lista1 passa a apontar para o primeiro elemento da lista2
+    return 1;
+}
