602b4991e5379c1eedbbae91eb12e58d8de9150f
jão <159722770+jpaullopes@users.noreply.github.com>
2024-11-15 10:56:30 -0300

    Update dinamicaEncadeada.h

--- listaDinamicaEncadeada/dinamicaEncadeada.h
+++ listaDinamicaEncadeada/dinamicaEncadeada.h
@@ -1,3 +1,4 @@
+
 //prototipos das funções
 //Tipos de dados a serem armazenadas na lista
 //ponteiro para o próximo elemento da lista
@@ -8,14 +9,20 @@ typedef struct aluno{
     float n1, n2, n3;
 }Aluno;
 
-typedef struct elemento* Lista;
+struct elemento{
+    Aluno dados;
+    struct elemento *proximo;
+};
+typedef struct elemento *Lista;
 
 //cria uma lista vazia
-Lista* criarLista();
+Lista* criarLista(void);
 //Liberar Lista
 void liberarLista(Lista* li);
 //Saber o tamanho da lista
 int tamanhoLista(Lista *li);
+//Retornar o tamanho da lista(Usando recursividade)
+int tamanhoListaRecursivo(Lista li);
 //Saber se lista está vazia
 int listaVazia(Lista *li);
 //Saber se lista está cheia
@@ -24,3 +31,19 @@ int listaCheia(Lista *li);
 int inserirComecoLista(Lista *li, Aluno al);
 //insere no final
 int inserirFinalLista(Lista *li, Aluno al);
+//inserção ordenada
+int inserirListaOrdenada(Lista *li, Aluno al);
+//remoção no inicio da lista
+int removerComecoLista(Lista *li);
+//remoção no final da lista
+int remocaoFinalLista(Lista *li);
+//remoção lista ordenadamente com base na matricula
+int remocaoListaOrdenada(Lista *li, int mat);
+//consulta dados de um aluno com base na matricula
+int consultaListaMatricula(Lista *li, int mat, Aluno *al);
+//consulta dados de um aluno com base na posição
+int consultaListaPosicao(Lista *li,int pos, Aluno *al);
+//exibe a lista
+void exibirLista(Lista li); //tratando sem ponteiro e sim como uma copia da lista
+//Anexação de listas encadeadas
+int anexarListas(Lista *lista1, Lista *lista2);
